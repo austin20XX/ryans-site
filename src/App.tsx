3dc923ccdcd0020d8aa28812a2bc46763e5dd931
austin_m <1austinm@protonmail.com>
2024-04-08 18:05:12 -0500

    feat: added react-helmet for head tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // Passing an empty array for the second param, tells React effect doesn't depend on anything
-  // This way it will never needs to re-run
-  // Equivalent to componentDidMount
-  useEffect(() => {
-    document.title = '(NEW TITLE HERE)';
-  }, []);
-
-
   return (
     <div className="App">
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <title> (NEW TITLE HERE) </title>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
