8d800dad5c034070ef06329d84ac88526edf2985
austin_m <1austinm@protonmail.com>
2024-04-08 17:39:57 -0500

    feat: added document title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // Passing an empty array for the second param, tells React effect doesn't depend on anything
+  // This way it will never needs to re-run
+  // Equivalent to componentDidMount
+  useEffect(() => {
+    document.title = '(NEW TITLE HERE)';
+  }, []);
+
+
   return (
     <div className="App">
     </div>
