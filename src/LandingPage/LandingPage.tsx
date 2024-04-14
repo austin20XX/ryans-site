573977b69c605a7dd500b8b9ba2b4ce0c9a53e16
austin_m <1austinm@protonmail.com>
2024-04-14 17:14:49 -0500

    feat: landing page basics

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,11 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import  RyanFullBody from '../assets/RyanFullBody.png'
+
 interface LandingPageProps {
     exceptionThrown?: boolean
 }
 
 export const LandingPage = ({exceptionThrown, ...props}: LandingPageProps) => {
     return (
-        <div>
-            Testing
-        </div>
+        <Box id='landing-page'
+             maxWidth='xl'
+             sx={{display: 'flex', justifyContent: 'space-evenly'}}
+        >
+            <Box id='left-split'><img src={RyanFullBody} alt='Ryan Watts Full Body Shot>' /> </Box>
+            <Box id='right-split'>
+                <Typography variant="h2">Ryan Watts</Typography>
+                <Typography variant="h2">Photographer</Typography>
+                <Typography variant="h2">Videographer</Typography>
+                <Typography variant="h2">Chicago Confessionals Co-Founder</Typography>
+            </Box>
+        </Box>
     )
 };
